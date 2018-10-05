e8868bc3a69278b8d42ce26a50967c1a1121fb26
Jakob de Maeyer <jakobdm1@gmail.com>
2018-10-05 14:28:15 +0200

    Add REST interface via flask-potion

--- severus/app.py
+++ severus/app.py
@@ -1,11 +1,31 @@
 from flask import Flask
+from flask_potion import Api, ModelResource
+from flask_potion.contrib.peewee import PeeweeManager
 
+from severus.db import User, Task
 from severus.utils import get_version
 
 
 app = Flask('severus')
 
 
+api = Api(app, default_manager=PeeweeManager)
+
+
 @app.route('/')
 def root():
     return get_version()
+
+
+class UserResource(ModelResource):
+    class Meta:
+        model = User
+
+
+class TaskResource(ModelResource):
+    class Meta:
+        model = Task
+
+
+api.add_resource(UserResource)
+api.add_resource(TaskResource)
